f4161ad603ef0f8e995ecaa8a231c6ad5174c0ee
rx-ted <1552488649@qq.com>
2023-01-03 21:44:42 +0800

    del *.o and *.d *.map

--- project/led点灯/Objects/cpuport.d
+++
@@ -1,8 +0,0 @@
-./objects/cpuport.o: ..\..\rt-thread\libcpu\cortex-m4\cpuport.c \
-  ..\..\include\rtthread.h user\inc\rtconfig.h ..\..\include\rtdebug.h \
-  ..\..\include\rtdef.h C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdarg.h \
-  ..\..\include\rtlibc.h ..\..\include\libc\libc_stat.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\time.h \
-  ..\..\include\libc\libc_errno.h ..\..\include\rtservice.h \
-  ..\..\include\rtm.h
